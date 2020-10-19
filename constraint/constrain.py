c0fd4fa3bd29bc157fb543e24acdf670b865223a
Jeff Richley <jeffrichley@gmail.com>
2020-10-18 23:08:42 -0400

    updated the constraint API to take a pandas dataframe

--- constraint/constrain.py
+++ constraint/constrain.py
@@ -1,9 +1,81 @@
 import constraint
 import math
+import pandas as pd
 
 # this requires the constraint library.  It can be installed by using 'pip install python-constraint'
 
 def get_solution(peaches):
+    '''
+    This method will take a list of tuples representing the peaches and return a list of 1's and 0's representing
+    keeping and pruning.  THe 1's show a peachlet will be kept and 0's indicate the need for pruning.
+    :param peaches: A list of tuples, with each tuple representing a peachlet of the form (x, y, z, size).
+    :return: A list of 0's and 1's for prune and keep
+    '''
+
+    original_peaches = peaches.copy()
+
+    distances = {}
+    # for peach in peaches:
+    for peach_num, peach in peaches.iterrows():
+        distances[peach_num] = {}
+        # for peach_two in peaches:
+        for peach_two_num, peach_two in peaches.iterrows():
+            distances[peach_num][peach_two_num] = math.sqrt((peach['X'] - peach_two['X'])**2 +
+                                                            (peach['Y'] - peach_two['Y'])**2 +
+                                                            (peach['Z'] - peach_two['Z'])**2)
+
+    def distance_constraint(*args):
+
+        for i in range(len(args)):
+            if args[i] == 1:
+                for j in range(i + 1, len(args)):
+                    if args[j] == 1:
+                        if distances[i][j] < 6:
+                            # nope the two peachlets are too close
+                            return None
+
+        # looks like a good one
+        return True
+
+    # define our problem
+    problem = constraint.Problem()
+
+    peach_ids = []
+
+    # each one of the peaches can be 0 pick or 1 (leave)
+    for peach_num, peach in peaches.iterrows():
+        problem.addVariable(peach_num, [0, 1])
+        peach_ids.append(peach_num)
+
+    # everyone needs to be at least 6 inches away
+    problem.addConstraint(distance_constraint, peach_ids)
+
+    # get all the solutions
+    solutions = problem.getSolutions()
+
+    # now find the solution with the largest mass of total kept peaches
+    largest = -1
+    best_solution = None
+    for solution in solutions:
+        size = 0
+        for i in solution:
+            if solution[i] == 1:
+                size += original_peaches.iloc[i]['Radius']
+
+        if size > largest:
+            best_solution = solution
+            largest = size
+
+    answer = peaches.copy()
+    prune = []
+    for one in best_solution:
+        prune.append(best_solution[one] == 0)
+
+    answer['Remove'] = prune
+
+    return answer
+
+def get_solution_old(peaches):
     '''
     This method will take a list of tuples representing the peaches and return a list of 1's and 0's representing
     keeping and pruning.  THe 1's show a peachlet will be kept and 0's indicate the need for pruning.
@@ -20,9 +92,9 @@ def get_solution(peaches):
         peach_num = peach[4]
         distances[peach_num] = {}
         for peach_two in peaches:
-            distances[peach_num][peach_two[4]] = math.sqrt((peach[0] - peach_two[0])**2 +
-                                                           (peach[1] - peach_two[1])**2 +
-                                                           (peach[2] - peach_two[2])**2)
+            distances[peach_num][peach_two[4]] = math.sqrt((peach[0] - peach_two[0]) ** 2 +
+                                                           (peach[1] - peach_two[1]) ** 2 +
+                                                           (peach[2] - peach_two[2]) ** 2)
 
     def distance_constraint(*args):
 
--- constraint/constraint_test.py
+++ constraint/constraint_test.py
@@ -1,12 +1,46 @@
 from constrain import *
+import pandas as pd
+import random
+
+num_peaches = 15
+distance = 18
+max_size = 10
 
 # each peach is represented by a tuple that is (x, y, z, size)
-peachlets = [(0, 0, 0, 1),
-             (0, 0, 1, 10),
-             (0, 0, 1, 2),
-             (0, 0, 6, 1),
-             (0, 0, 6.2, 5),
-             (0, 0, 13, 3),
-             (0, 0, 13, 4)]
+peachlets = []
+for i in range(num_peaches):
+    peach = (0, 0, random.uniform(0, 1) * distance, random.uniform(0, 1) * max_size)
+    peachlets.append(peach)
 
-get_solution(peachlets)
+peachlets.sort()
+
+print('***********************')
+print(peachlets)
+print('***********************')
+print(get_solution_old(peachlets))
+
+x = []
+y = []
+z = []
+radius = []
+pose = [1 for x in range(num_peaches)]
+remove = [False for x in range(num_peaches)]
+for i in range(num_peaches):
+    peach = peachlets[i]
+    x.append(peach[0])
+    y.append(peach[1])
+    z.append(peach[2])
+    radius.append(peach[3])
+
+
+d = {'X': x,
+     'Y': y,
+     'Z': z,
+     'Radius': radius,
+     'Pose': pose,
+     'Remove': remove}
+peachlets = pd.DataFrame(data=d)
+
+picking = get_solution(peachlets)
+
+print(picking)
